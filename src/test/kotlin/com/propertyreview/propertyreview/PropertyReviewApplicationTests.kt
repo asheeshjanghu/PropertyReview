84de3437ed20d4cf835185438873a6de3ce843e6
asheeshtyro <ajanghu@tyro.com>
2021-07-11 18:14:02 +1000

    Initial commit with bare minimum

---
+++ src/test/kotlin/com/propertyreview/propertyreview/PropertyReviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.propertyreview.propertyreview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PropertyReviewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
